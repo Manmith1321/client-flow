930896a23f5538b1b5f3b5944db99eae2518e730
Manmith Vardhan Challa <manmithvardhanchalla@Manmiths-MacBook-Air.local>
2026-06-14 02:21:40 +0530

    Configure Neon database

--- config/db.js
+++ config/db.js
@@ -9,7 +9,13 @@ const sequelize = new Sequelize(
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: 'postgres',
-    logging: false, // Set to console.log to see SQL queries
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
+    logging: false,
   }
 );
 
